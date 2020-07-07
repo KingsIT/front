1467901fe1b89a71f5931212496dd0b21526412b
Gim <jjqian@creams.io>
2020-07-07 18:13:06 +0800

    feat: index.css -> index.scss

--- src/container/views/Home/index.tsx
+++ src/container/views/Home/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { a_io } from './actions'
 import mk from '../../../../build/webpack/webpack.md';
+import styles from './index.scss';
 
 export default function Home() {
     return (
-        <div onClick={() => { a_io() }}>
+        <div onClick={() => { a_io() }} className={styles.test}>
             <div dangerouslySetInnerHTML={{__html: mk}}></div>
         </div>
-        )
-    }
+    )
+}
