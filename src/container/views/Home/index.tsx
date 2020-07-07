1e7526072da806c9e5b13e0bbef520e7966af37c
Gim <jjqian@creams.io>
2020-07-07 16:36:25 +0800

    feat: mkdown-html loader

--- src/container/views/Home/index.tsx
+++ src/container/views/Home/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { a_io } from './actions'
+import mk from '../../../../build/webpack/webpack.md';
 
 export default function Home() {
     return (
-        <div onClick={() => { a_io() }}>Home555</div>
-    )
-}
+        <div onClick={() => { a_io() }}>
+            <div dangerouslySetInnerHTML={{__html: mk}}></div>
+        </div>
+        )
+    }
