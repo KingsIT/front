4cdc64e3125bd9b02b63f3e387fbcc08bbb0d5bc
Gim <jjqian@creams.io>
2020-10-19 09:11:25 +0800

    feat: 打包配置修改

--- build/plugins.js
+++ build/plugins.js
@@ -3,10 +3,10 @@ const webpack = require('webpack');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const {resolveAssetsRootDir} = require('./utils');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { LifeCycleWebpackPlugin } = require('lifecycle-webpack-plugin');
-const MyPlugin = require('./customerJs/myPlugin');
-const InOrNotINPlugin = require('./customerJs/InOrNotINPlugin');
-const LifeCofPlugin = require('./customerJs/lifeCofPlugin');
+// const { LifeCycleWebpackPlugin } = require('lifecycle-webpack-plugin');
+// const MyPlugin = require('./customerJs/myPlugin');
+// const InOrNotINPlugin = require('./customerJs/InOrNotINPlugin');
+// const LifeCofPlugin = require('./customerJs/lifeCofPlugin');
 const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
 
 const mode = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
@@ -43,33 +43,33 @@ const pluginDev = [
             inject: true,
             hash: true,
         }),
-        new HtmlWebpackPlugin({
-            inject: true,
-            // hash: true,
-            // filename: 'resume.html',
-            template: 'build/resume.html',
-            title: '放飞自我的介绍',
-            chunks: ['resume'], // 添加多入口对应的chunk 到template 文件中
-            minify: {
-                removeComments: true,
-                collapseWhitespace: true,
-                removeAttributeQuotes: true,
-            }
-        }),
+        // new HtmlWebpackPlugin({
+        //     inject: true,
+        //     // hash: true,
+        //     // filename: 'resume.html',
+        //     template: 'build/resume.html',
+        //     title: '放飞自我的介绍',
+        //     chunks: ['resume'], // 添加多入口对应的chunk 到template 文件中
+        //     minify: {
+        //         removeComments: true,
+        //         collapseWhitespace: true,
+        //         removeAttributeQuotes: true,
+        //     }
+        // }),
         new webpack.HotModuleReplacementPlugin(),
         new MiniCssExtractPlugin({
             filename: resolveAssetsRootDir('css/[name].css'),
         }),
         new CleanWebpackPlugin(),
-        new LifeCycleWebpackPlugin({
-            done: (compiler) => {
-                // console.log(compiler, new Date(), '=====');
-            },
-        }),
-        new MyPlugin(),
-        new LifeCofPlugin(),
+        // new LifeCycleWebpackPlugin({
+        //     done: (compiler) => {
+        //         // console.log(compiler, new Date(), '=====');
+        //     },
+        // }),
+        // new MyPlugin(),
+        // new LifeCofPlugin(),
         new BundleAnalyzerPlugin(),
-        new InOrNotINPlugin(),
+        // new InOrNotINPlugin(),
     ]
 
 module.exports = mode === 'production' ? pluginPro : pluginDev;
